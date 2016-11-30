2ba6a76816b6aa82fdd6a00a8162f3618eabfed2
Leonardo Volthier Barbosa Gomes <volthier@gmail.com>
2016-11-30 14:44:51 -0200

    [UPDATE] Organização de pasta da aplicação e Atualização de declaração Js relatorios.

---
+++ src/main/resources/static/js/relatorios/relatorios.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	
+//Requisição Json do tabela Chips	
+	$.ajax({
+		type: 'GET',
+		headers: {
+			Accept : "application/json; charset=utf-8",
+			"Content-Type": "application/json; charset=utf-8"
+		},
+	
+		url : '//localhost:8080/relatorio/chips' ,
+		success : function(result){
+			google.charts.load('current',{'packages' : ['corechart']
+			});
+			google.charts.setOnLoadCallback(function(){
+				drawChart(result);
+			});
+		}
+	});
+	
+//Função de desenho das informações requisitadas para objetos JS.
+function drawChart(result){
+		
+		var data = new google.visualization.DataTable();
+		data.addColumn('string','tipoChip');
+		data.addColumn('string','numeroSerieChip');
+		data.addColumn('number','idChip');
+		
+		var dataArray = [];
+		$.each(result, function(i,obj){
+			dataArray.push([obj.tipoChip, obj.numeroSerieChip, obj.idChip]);
+		});
+		
+		data.addRows(dataArray);
+		
+		
+		var piechart_options = {
+				title : 'Pie Chart: How Much Products Sold By Last Night',
+				width: 400,
+				height: 300
+		};
+		
+		var piechart = new google.visualization.PieChart(document
+				.getElementById('piechart_div'));
+		piechart.draw(data, piechart_options);
+		
+		var barchart_options = {
+				title : 'Bar Chart: How Much Products Sold By Last Night',
+				width: 400,
+				height: 300,
+				legend: 'none'
+		};
+		
+		var barchart = new google.visualization.BarChart(document
+				.getElementById('barchart_div'));
+		barchart.draw(data, barchart_options);	
+	}
+});
