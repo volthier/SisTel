2ae9f7d88ec4087fd1641f9ab47cf1b05add7219
chris-redfield <christianmoryah@gmail.com>
2017-02-03 19:05:13 -0200

    [update] adicionando uma consulta inicial de alocações na tela inicial

--- src/main/java/br/gov/cultura/DitelAdm/controller/UrlController.java
+++ src/main/java/br/gov/cultura/DitelAdm/controller/UrlController.java
@@ -1,16 +1,30 @@
 package br.gov.cultura.DitelAdm.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.gov.cultura.DitelAdm.model.dtos.AlocacaoLinhaDispositivoUsuarioDTO;
+import br.gov.cultura.DitelAdm.service.AlocacaoService;
+
+
 
 @Controller
 @RequestMapping
 public class UrlController {
 
+@Autowired
+private AlocacaoService alocacaoService;
+	
 @RequestMapping("/login")
 public ModelAndView login(@RequestParam(value = "error",required = false) String error,
 @RequestParam(value = "/logout",	required = false) String logout,RedirectAttributes attributes) {
@@ -29,7 +43,42 @@ public ModelAndView login(@RequestParam(value = "error",required = false) String
 
 	@RequestMapping("/inicio")
 	public ModelAndView inicio(){
-		ModelAndView mv = new ModelAndView("TelaInicio");	
+		List<Object[]> linhaDispo =  alocacaoService.getAlocacaoUsuarioLinhaList();
+		List<AlocacaoLinhaDispositivoUsuarioDTO> lista = new ArrayList<AlocacaoLinhaDispositivoUsuarioDTO>();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		for (Object[] item : linhaDispo) {
+	    	Date dtDevolucao = null;
+	    	Date dtRecebido = null;
+	    	
+			if(item[2] != null){
+				try {
+					dtDevolucao = df.parse(item[2].toString().substring(0, 19));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+			if(item[3] != null){
+				try {
+					dtRecebido = df.parse(item[3].toString().substring(0, 19));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+			}
+	    	
+	    	AlocacaoLinhaDispositivoUsuarioDTO alocacao = 
+	    			new AlocacaoLinhaDispositivoUsuarioDTO(
+	    					Integer.parseInt(item[0].toString()),
+	    					Integer.parseInt(item[1].toString()),
+	    					dtDevolucao,
+	    					dtRecebido,
+	    					Integer.parseInt(item[4].toString()),
+	    					Integer.parseInt(item[5].toString()));
+	    	lista.add(alocacao);
+	    }
+		
+		ModelAndView mv = new ModelAndView("TelaInicio");
+		mv.addObject("alocacao",lista);
 		return mv;
 	}
 	
