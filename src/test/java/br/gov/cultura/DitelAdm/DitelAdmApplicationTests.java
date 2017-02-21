054289026b4d33161ed3dec38e77f7c272517fa1
chris-redfield <christianmoryah@gmail.com>
2017-02-21 20:08:14 -0300

    [update] removendo teste de build no POM e teste que não faz nada da classe de teste

--- src/test/java/br/gov/cultura/DitelAdm/DitelAdmApplicationTests.java
+++ src/test/java/br/gov/cultura/DitelAdm/DitelAdmApplicationTests.java
@@ -6,12 +6,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DitelAdmApplication.class)
-@WebAppConfiguration
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = DitelAdmApplication.class)
+//@WebAppConfiguration
 public class DitelAdmApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
