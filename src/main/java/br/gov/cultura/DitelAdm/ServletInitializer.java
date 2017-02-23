dec8cb346e9f2cce47dc17cdec1ea8f487c84965
chris-redfield <christianmoryah@gmail.com>
2017-02-22 21:23:50 -0300

    [fix(porra aval)] restaurando servlet initializer - requisito para jboss / wildfly

---
+++ src/main/java/br/gov/cultura/DitelAdm/ServletInitializer.java
@@ -0,0 +1,12 @@
+package br.gov.cultura.DitelAdm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DitelAdmApplication.class);
+	}
+}
