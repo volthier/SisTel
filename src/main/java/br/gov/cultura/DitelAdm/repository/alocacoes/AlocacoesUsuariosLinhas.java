7d5c4ecbaf5c8b063de94b3ffc494cdd2d6372f2
chris-redfield <christianmoryah@gmail.com>
2017-02-23 13:50:00 -0300

    [update] src/main/java/br/gov/cultura/DitelAdm/repository/alocacoes/AlocacoesUsuariosLinhas.java

--- src/main/java/br/gov/cultura/DitelAdm/repository/alocacoes/AlocacoesUsuariosLinhas.java
+++ src/main/java/br/gov/cultura/DitelAdm/repository/alocacoes/AlocacoesUsuariosLinhas.java
@@ -14,8 +14,8 @@ public interface AlocacoesUsuariosLinhas extends JpaRepository<AlocacaoUsuarioLi
 	public AlocacaoUsuarioLinha findByIdAlocacaoUsuarioLinha(Integer idAlocacaoUsuarioLinha);
 	
 	@Query(value="SELECT alocacaoLinhaDispositivo.dispositivo_id_dispositivo, alocacaoUsuarioLinha.* "
-			+ "     FROM diteladmdev.alocacao_linha_dispositivo AS alocacaoLinhaDispositivo ,"
-			+ " 		 diteladmdev.alocacao_usuario_linha AS alocacaoUsuarioLinha "
+			+ "     FROM dbditel.alocacao_linha_dispositivo AS alocacaoLinhaDispositivo ,"
+			+ " 		 dbditel.alocacao_usuario_linha AS alocacaoUsuarioLinha "
 			+ " WHERE alocacaoLinhaDispositivo.id_alocacao_linha_dispositivo = alocacaoUsuarioLinha.id_alocacao_usuario_linha", nativeQuery = true)
 	public List<Object[]> getAlocacoesUsuarios ();
 	
