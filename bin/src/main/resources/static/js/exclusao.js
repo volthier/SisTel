90b01ff0def5413edfadacb1cbca05333c48e479
leonardo <volthier@gmail.com>
2017-03-01 15:03:05 -0300

    [FIX] Readequação de templates e configuração do link de acesso do servidor de aplicação.(/ROOT)

---
+++ bin/src/main/resources/static/js/exclusao.js
@@ -0,0 +1,90 @@
+/*Script caledario input*/
+/*$('.input-group.date').datepicker({
+    format: "dd/mm/yyyy HH:mm:ss",
+    todayBtn: true,
+    clearBtn: true,
+    language: "pt-BR",
+    daysOfWeekDisabled: "0",
+    daysOfWeekHighlighted: "0",
+    autoclose: true,
+    todayHighlight: true
+});*/
+
+/* TESTE CALENDARIO*/
+  
+	  $('#datetimepicker').datetimepicker({
+		  format: 'yyyy/mm/dd hh:ii',
+	    
+	    todayBtn: true,
+	    clearBtn: true,
+	    language: "pt-BR",
+	    daysOfWeekDisabled: "0",
+	    daysOfWeekHighlighted: "0",
+	    autoclose: true,
+	    todayHighlight: true
+	  });
+
+/*Script de exclusao(Botão)*/
+$('#confirmacaoExclusaoModal').on('shown.bs.modal', function(event){
+	
+	var button = $(event.relatedTarget);
+		
+	var id = button.data('excludid');
+	var item = button.data('exclude');
+	var flag = button.data('flag')
+	
+	var modal = $(this);
+	
+	var form = modal.find('form');
+	var action = form.data('url-consulta');
+	
+	if(flag == 1){
+		action ='/Sistel/dispositivos';
+	if(!action.endsWith('/')){
+		action +='/' ;
+	}
+	form.attr('action', action + id); 
+	
+	modal.find('.modal-body span').html('Confirma a exclusão do registro<strong> '+ item +' </strong>?' );
+
+	}
+	else if(flag == 2){
+		action ='/Sistel/usuarios';
+		if(!action.endsWith('/')){
+			action +='/' ;
+		}
+		form.attr('action', action + id); 
+		
+		modal.find('.modal-body span').html('Confirma a exclusão do registro<strong> '+ item +' </strong>?' );
+				
+	}
+	
+	else if(flag == 3){
+		action ='/Sistel/chips';
+		if(!action.endsWith('')){
+			action +='/' ;
+		}
+		form.attr('action', action + id); 
+		
+		modal.find('.modal-body span').html('Confirma a exclusão do registro<strong> '+ item +' </strong>?' );
+				
+	}
+	else if(flag == 4){
+		action ='/Sistel/linhas';
+		if(!action.endsWith('/')){
+			action +='/' ;
+		}
+		form.attr('action', action + id); 
+		
+		modal.find('.modal-body span').html('Confirma a exclusão do registro<strong> '+ item +' </strong>?' );
+				
+	}
+	
+	});
+
+
+/*Script do Menu (Anchor)*/
+$('.menu-anchor').on('click touchstart', function(e){
+	$('html').toggleClass('menu-active');
+  	e.preventDefault();
+});
