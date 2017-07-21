9e71c6a96a8aba17c6cb256f0d6194eedf65010e
leonardo <volthier@gmail.com>
2017-07-21 09:49:43 -0300

    [UPDATE] Arquivo de configuração de transaction JPA.

---
+++ src/main/java/br/gov/cultura/DitelAdm/config/JPAConfiguration.java
@@ -0,0 +1,19 @@
+package br.gov.cultura.DitelAdm.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+}
