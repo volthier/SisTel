487168a9d2521ecc70dab94975dc02e4f27715d6
leonardo <volthier@gmail.com>
2017-07-26 16:10:53 -0300

    [UPDATE] Atualização de Favicon.

--- src/main/java/br/gov/cultura/DitelAdm/config/WebSecurityConfig.java
+++ src/main/java/br/gov/cultura/DitelAdm/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-		.antMatchers("/css/**", "/js/**", "/img/**", "/images/**", "/resources/**", "/webjars/**","/fonts/**","/miminium/**").permitAll();
+		.antMatchers("/css/**", "/js/**", "/img/**", "/images/**", "/resources/**", "/webjars/**","/fonts/**","/miminium/**","/icon/**").permitAll();
 		
 		http
 		.authorizeRequests()
