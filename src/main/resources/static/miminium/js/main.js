fe507fb3f940a38a6cdc3c4ac234f32d1a3d8000
luizfpinheiro <luiz.pinheiro@cultura.gov.br>
2017-09-08 17:50:19 -0300

    [FIX]Alteração de layout para Cadastrar Fatura.

--- src/main/resources/static/miminium/js/main.js
+++ src/main/resources/static/miminium/js/main.js
@@ -157,7 +157,13 @@
 
 	// end: Chart =============
 
-
+	$(document).on('click', '.browse', function(){
+		  var file = $(this).parent().parent().parent().find('.file');
+		  file.trigger('click');
+		});
+		$(document).on('change', '.file', function(){
+		  $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+		});
 	
 	$.fn.ripple = function() {
 		$(this)
